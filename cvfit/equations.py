Fixed Likelihood interval calculation

cvfit/equations.py
@@ -1,30 +1,61 @@
 """
 Equations available to fit data in CVFIT.
 """
+from math import sqrt, log, pi
 import copy
 import numpy as np
-def SSD(gars, func, data, notfixed, fixval):
+from scipy import optimize
+
+def SSDlik(theta, func, data, notfixed, allpars=None):
+    """
+    Calculate likelihood coresponding to the sum of the squared deviations 
+    assuming that errors follow Gausian distribution.
+    """
+
+    if allpars != None:
+        pars = []
+        j = 0
+        for i in range(len(allpars)):
+            if notfixed[i]:
+                pars.append(theta[j])
+                j += 1
+            else:
+                pars.append(allpars[i])
+    else:
+        pars = theta
+    S = 0.0
+    for point in data.points:
+        S += (point.y - func(pars, point.x)) ** 2
+    nfit = data.size()
+    kfit = np.nonzero(notfixed)[0].size
+    Sres = sqrt(S / (nfit - kfit))
+    return nfit * log(sqrt(2 * pi) * Sres) + S / (2 * Sres**2)
+
+def SSDlik_contour(x, num, pars, notfixed, func, dataset):
+    fixfix = copy.deepcopy(notfixed)
+    fixfix[num] = False
+    allpars = pars.copy()
+    allpars[num] = x
+    theta = allpars[np.nonzero(fixfix)[0]]
+    result = optimize.minimize(SSDlik, theta, 
+        args=(func, dataset, fixfix, allpars), 
+        method='Nelder-Mead', jac=None, hess=None)
+    return -result.fun, result.x
+
+def SSD(args, func, data):
     """
     Calculate sum of squared deviations.
     """
-   # print '\n data \n', data 
     S = 0.0
-   # if type(data) is np.ndarray:
-   #     S = 2
-   # else:
     for point in data.points:
-        S += (point.y - func(gars, point.x, notfixed, fixval)) ** 2
+        S += (point.y - func(args, point.x)) ** 2
     return S
-    
-def hill_equation(gars, conc, notfixed, fixval):
+
+def hill_equation(args, conc):
     '''
     The hill equation.
     '''
-    #print '\n gars, notfixed \n', gars, notfixed
-    for i in range(0,len(gars)):
-        if not notfixed[i]:
-            gars[i] = copy.deepcopy(fixval[i])
-    ymin, ymax, ec50, nH = gars
+    ymin, ymax, ec50, nH = args
     return ymin + (ymax * (conc / ec50) ** nH) / (1 + (conc / ec50) ** nH)
     
 

cvfit/errors.py
@@ -1,52 +1,40 @@
 import numpy as np
 from numpy import linalg as nplin
-from equations import SSD
-from scipy import stats
-from scipy import optimize
-import matplotlib.pyplot as plt
-import copy
-import math as math
+from equations import SSD, SSDlik, SSDlik_contour
+from math import fabs, sqrt
 
-def optimise_deltas(dataset, func, args, notfixed, fixval):
+def optimise_deltas(dataset, func, args, notfixed):
     """ """
-    Lmax = -0.5 * SSD(args, func, dataset, notfixed, fixval)
+    Lmax = -0.5 * SSD(args, func, dataset)
     Lcrit = 1.005 * Lmax
     deltas = np.zeros((len(args),))
     for index in np.nonzero(notfixed):
         deltas[index] += 0.1 * args[index]
-    L = -0.5 * SSD(args + deltas, func, dataset, notfixed, fixval)
+    L = -0.5 * SSD(args + deltas, func, dataset)
     if L > Lcrit:
         count = 0
         while L > Lcrit and count < 100:
             for index in np.nonzero(notfixed):
                 deltas[index] *= 2
-            L = -0.5 * SSD(args + deltas, func, dataset, notfixed, fixval)
+            L = -0.5 * SSD(args + deltas, func, dataset)
             count += 1
     elif L < Lcrit:
         count = 0
         while L < Lcrit and count < 100:
             for index in np.nonzero(notfixed):
                 deltas[index] *= 0.5
-            L = -0.5 * SSD(args + deltas, func, dataset, notfixed, fixval)
+            L = -0.5 * SSD(args + deltas, func, dataset)
             count += 1
     #print '\n deltas = \n', deltas
     return deltas
 
-def hessian(dataset, func, pars, notfixed, fixval):
+def hessian(dataset, func, pars, notfixed):
     """
     """
     kfit = np.nonzero(notfixed)[0].size
     hessian = np.zeros((kfit, kfit))
-    deltas = optimise_deltas(dataset, func, pars, notfixed, fixval)#deltas
-   # hayyam = np.zeros(50)#delete
-   # deltas = copy.deepcopy(ideltas)# delete
-   # for jar in range(-50, 50, 2): #delete
-        #print '\n jar \n', jar
-         #delete
-   #     pos = 2 #delete
-   #     deltas[pos] = copy.deepcopy(ideltas[pos])*(10**(jar/10)) #delete
+    deltas = optimise_deltas(dataset, func, pars, notfixed)
     i = 0
-    #print '\n deltas \n', deltas
     for i1 in np.nonzero(notfixed)[0]:
         j = 0
         for j1 in np.nonzero(notfixed)[0]:
@@ -56,9 +44,9 @@ def hessian(dataset, func, pars, notfixed, fixval):
                 coe1[j1] += deltas[j1]
                 coe3[j1] -= deltas[j1]
                 hessian[i, j] = ((
-                    SSD(coe1, func, dataset, notfixed, fixval) -
-                    2.0 * SSD(pars, func, dataset, notfixed, fixval) +
-                    SSD(coe3, func, dataset, notfixed, fixval)) /
+                    SSD(coe1, func, dataset) -
+                    2.0 * SSD(pars, func, dataset) +
+                    SSD(coe3, func, dataset)) /
                     (deltas[j1]  ** 2))
             else:
                 coe1[i1] += deltas[i1]
@@ -70,23 +58,19 @@ def hessian(dataset, func, pars, notfixed, fixval):
                 coe4[i1] -= deltas[i1]
                 coe4[j1] -= deltas[j1]
                 hessian[i, j] = ((
-                    SSD(coe1, func, dataset, notfixed, fixval) -
-                    SSD(coe2, func, dataset, notfixed, fixval) -
-                    SSD(coe3, func, dataset, notfixed, fixval) +
-                    SSD(coe4, func, dataset, notfixed, fixval)) /
+                    SSD(coe1, func, dataset) -
+                    SSD(coe2, func, dataset) -
+                    SSD(coe3, func, dataset) +
+                    SSD(coe4, func, dataset)) /
                     (4 * deltas[i1] * deltas[j1]))
             j += 1
         i += 1
-      #  hayyam[(jar+50)/2] = copy.deepcopy(hessian[pos-1, pos-1]) #delete
- #   print '\n deltaH SS = \n', hayyam
-        #plt.plot(jar, hayyam)#delete
-        #plt.show
     return 0.5 * hessian
 
-def covariance_matrix(dataset, func, pars, notfixed, fixval):
+def covariance_matrix(dataset, func, pars, notfixed):
     """ """
-    cov = nplin.inv(hessian(dataset, func, pars, notfixed, fixval))
-    minssd = SSD(pars, func, dataset, notfixed, fixval)
+    cov = nplin.inv(hessian(dataset, func, pars, notfixed))
+    minssd = SSD(pars, func, dataset)
     #print '\n SSD \n', minssd
     kfit = np.nonzero(notfixed)[0].size
     #TODO: check if weights were used to calculate SSD. If not then
@@ -94,21 +78,12 @@ def covariance_matrix(dataset, func, pars, notfixed, fixval):
     errvar = minssd / (dataset.size() - kfit)
     return cov * errvar
 
-def approximateSD(dataset, func, pars, notfixed, fixval):
+def approximateSD(dataset, func, pars, notfixed):
     """ """
-    cov = covariance_matrix(dataset, func, pars, notfixed, fixval)
+    cov = covariance_matrix(dataset, func, pars, notfixed)
     return np.sqrt(cov.diagonal())
     
 def correlation_matrix(covar):
-#    for i1 in range(len(covar)):
-#        j1=0
-#        for j1 in range(len(covar)):
-#            if i1==j1:
-#                den[i1] = copy.deepcopy(covar[i1,j1])
-#                elif i1>j1:
-#                    nom [i1,j1] = covar [i1,j1]
-#                else:
-#                    pass
     correl = np.zeros((len(covar),len(covar)))
     
     for i1 in range(len(covar)):
@@ -117,94 +92,86 @@ def correlation_matrix(covar):
             correl[i1,j1] = covar[i1,j1]/np.sqrt(np.multiply(covar[i1,i1],covar[j1,j1]))
     return correl
             
+def tvalue(ndf):
+    """
+    Return P=0.95 value of Student's t, with ndf degrees of freedom.
+    """
+    ttable = [12.706, 4.303, 3.182, 2.776, 2.571, 2.447, 2.365,
+        2.306, 2.262, 2.228, 2.201, 2.179, 2.160, 2.145, 2.131,
+        2.120, 2.210, 2.101, 2.093, 2.086, 2.080, 2.074, 2.069,
+        2.064, 2.060, 2.056, 2.052, 2.048, 2.045, 2.042]
+    if ndf in range(1, 31):
+        tval = ttable[ndf-1]
+    elif ndf in range(31, 41):
+        frac = (ndf - 30.0) / 10.0 #=0.1 to 1
+        tval = 2.042 - frac * (2.042 - 2.021)
+    elif ndf in range(41, 61):
+        frac = (ndf - 40.0) / 20.0 #=0.05 to 1
+        tval = 2.021 - frac * (2.021 - 2.000)
+    elif ndf in range(61, 121):
+        frac = (ndf - 60.0) / 60.0 #=1/60 to 1
+        tval = 2.000 - frac * (2.000 - 1.980)
+    elif ndf > 120:
+        tval = 1.96
+    else:
+        print ' ERROR IN TVALUE '
+    return tval           
+
+def lik_intervals(names, pars, SD, notfixed, m, func, dataset):
+    
+    Lmax = -SSDlik(pars, func, dataset, notfixed)
+    clim = sqrt(2. * m)
+    Lcrit = Lmax - m
+    Llimits = []
+    
+    i = 0
+    for j in range(len(pars)):
+        if notfixed[j]:
+            print('\nCalculating Lik limits for parameter- {0} = {1:.3f}'.format(names[j], pars[j]))
+            xhigh1 = pars[j]
+            #TODO: if parameter constrained to be positive- ensure that xlow is positive
+            xlow1 = pars[j] - 2 * clim * SD[i]
+            xlow2 = xhigh1
+            xhigh2 = pars[j] + 5 * clim * SD[i]
+            print('\tInitial guesses for lower limit: {0:.3f} and {1:.3f}'.format(xlow1, xhigh1))
+            print('\tInitial guesses for higher limit: {0:.3f} and {1:.3f}'.format(xlow2, xhigh2))
+
+            found = False
+            iter = 0
+            xlowlim, xhighlim = None, None
+            while not found and iter < 100: 
+                L, theta = SSDlik_contour(((xlow1 + xhigh1) / 2), j, pars,
+                    notfixed, func, dataset ) 
+                if fabs(Lcrit - L) > 0.01:
+                    if L < Lcrit:
+                        xlow1 = (xlow1 + xhigh1) / 2
+                    else:
+                        xhigh1 = (xlow1 + xhigh1) / 2
+                else:
+                    found = True
+                    xlowlim = (xlow1 + xhigh1) / 2
+                    print 'lower limit found: ', xlowlim
+                iter += 1
+
+            found = False
+            iter = 0   
+            while not found and iter < 100: 
+                #L1, L2, L3 = SSDlik_bisect(xlow2, xhigh2, j, pars, notfixed, hill_equation, dataset)
+                L, theta = SSDlik_contour(((xlow2 + xhigh2) / 2), j, pars,
+                    notfixed, func, dataset ) 
+                if fabs(Lcrit - L) > 0.01:
+                    if L > Lcrit:
+                        xlow2 = (xlow2 + xhigh2) / 2
+                    else:
+                        xhigh2 = (xlow2 + xhigh2) / 2
+                else:
+                    found = True
+                    xhighlim = (xlow2 + xhigh2) / 2
+                    print 'higher limit found: ', xhighlim
+                iter += 1
 
 
-    
+            Llimits.append([xlowlim, xhighlim])
+            i += 1
 
-    
-def new_secs3(aSD, covar, dataset, func, pars, notfixed, hes):
-    #bear in mind that "true" likelihood distribution should be inverted.Here it isnt
-    fixval = copy.deepcopy(pars) #values which the pars are to be fixed at if true
-    fixfix = copy.deepcopy(notfixed)
-    
-    minssd = SSD(pars, func, dataset, notfixed, fixval)
-    kfit = np.nonzero(notfixed)[0].size
-    #TODO: check if weights were used to calculate SSD. If not then
-    # calculate errvar.
-    errvar = minssd / (dataset.size() - kfit)
-    pi = math.pi
-    slik = kfit * np.log(np.sqrt(2*pi)*errvar)
-    ELMAX = slik+(minssd/(2*np.sqrt(errvar)**2))
-    print '\n errvar, slik, ELMAX \n', errvar, slik, ELMAX
-    df = len(covar)
-    t = stats.t.ppf(0.95, df)
-    aaSD = approximateSD(dataset, func, pars, notfixed, fixval)
-    j = len(pars) - len(aaSD) #correcting for discrepancy between parameter array and freepar number
-    i = 2 #parameter which the intervals are sought for
-    
-    Lmaxm = 0.5*SSD(pars, func, dataset, notfixed, fixval)/errvar
-    Lmaxn = copy.deepcopy(Lmaxm) #just a deep copy for robustness during testing  
-    m = (t**2)*0.5
-    print '\n m \n', m
-    
-    contour = Lmaxn-m
-    
-    rile = [1,-1] # either go RIght from Smin or LEft, switches by g in the for loop
-    #result placeholders
-    RES = np.zeros(4)
-    RES1 = np.zeros(4)
-    RES2 = np.zeros(4)
-    fixfix[i+j] = not (i+j) #this tells that parameter 'i' is sought intervals for, and allows to fix it in minimise
-    #this is specific to cell, just needed for the "optimize" routine - to check for consistency
-    guess = [  0.00000000e+00,   9.48333333e+03,   1.73205081e+03,   9.97865711e-01]
-#    response = [  980.,  3193.,  4806.,  4949.,  6162.,  6183.,  6188.,  6190.,  6355.,  8658., 9000.,  9165.,  9276.,  9284.,  9492.,  9674.]
-#    weight = [ 1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.]
-#    concentration = [   100.,    300.,    600.,   1000.,   1000.,   1000.,   1000.,   1000.,   1000.,   3000.,  10000.,  10000.,  10000.,  30000.,  30000.,  30000.] 
-#
-
-    for g in range (0,1):
-        SDel = copy.deepcopy(pars)
-        print '\n SDel \n', SDel        
-        k = 1
-        STEP = float(aaSD[i]*2)/float((2**k))
-        lohi = copy.deepcopy(rile[g])
-        count = 0
-        #LGuess = Lmaxn + 3*SD
-        SDel[i+j] += lohi * STEP
-        print '\n STEP, lohi, SDel, fixfix \n', STEP, lohi, SDel, fixfix
-        
-        Lcurall = optimize.minimize(SSD, guess, args=(func, dataset, fixfix, SDel), method='Nelder-Mead', jac=None, hess=None)
-        Lcurx = copy.deepcopy(Lcurall.x) #this just takes the optimised parameters from Lcurall
-        Lcurx = np.asarray(Lcurx)
-        Lcur = SSD(Lcurx, func, dataset, fixfix, Lcurx)
-        #print '\n Lcur, Lcurx, contour \n', Lcur, Lcurx, contour
-        while np.sqrt(((0.5*Lcur / errvar) - contour)**2) >  0.005*Lmaxn/errvar and count<100:
-            if (0.5 * Lcur) - contour > 0:
-                n = -1
-            else:
-                n = 1
-            k+=1
-           # print '\n Step = \n', STEP
-            SDel[i+j] += lohi * n * STEP
-            STEP = float(2 * aaSD[i])/float((2**k))
-            Lcurx[i+j] += lohi * n * STEP
-            Lcurall = optimize.minimize(SSD, guess, args=(func, dataset, fixfix, Lcurx), method='Nelder-Mead', jac=None, hess=None)
-            Lcurx = copy.deepcopy(Lcurall.x)
-            Lcurx = np.asarray(Lcurx)
-            Lcur = SSD(Lcurx, func, dataset, fixfix, Lcurx)
-                      
-            count += 1 
-            #this if is just for debug now, basically: if 0=lower, 1=upper
-            if g == 0:
-                RES1 = copy.deepcopy(Lcurx)
-#                for m in range (0,4):
-#                    RES1[m] = RES1[m] - pars[m]
-                count1 = copy.deepcopy(count)
-            else:
-                RES2 = Lcurx
-            #FIX AASDEL and make a CHANGEABLE ONE
-    return RES, RES1, RES2, count1
-
-    
-
-            
+    return Llimits

test_hessian.py
@@ -1,9 +1,12 @@
 #! /usr/bin/python
 
+from math import pi, log, sqrt, fabs
 import numpy as np
+from scipy import stats
+from scipy import optimize
 from cvfit import errors
 from cvfit import cfio
-from cvfit import equations
+from cvfit.equations import hill_equation, SSD, SSDlik, SSDlik_contour
 import copy
 
 __author__="User"
@@ -13,37 +16,67 @@ if __name__ == "__main__":
     
 #    filename = cfio.ask_for_file()
     filename = "Example/Example.csv"
-    dataset = cfio.read_sets_from_csv(filename)[4]
+    dataset = cfio.read_sets_from_csv(filename)[2]
     print dataset
     
     # fit: minimise SSD function and get optimal parameters
     #pars = np.array([0.1e-19, 11124.1, 773.322, 1.23857])#0
     #pars = np.array([0.1e-19, 8033.44, 758.689, 1.02105])#1
-    #pars = np.array([0.1e-19, 19237, 425.356, 1.35852])#2
+    pars = np.array([0.1e-19, 19237, 425.356, 1.35852])#2
     #pars = np.array([0.1e-19, 17073.8, 408.942, 1.34237])#3
-    pars = np.array([0.1e-19, 9616.72, 615.511, 1.11186])#4
+    #pars = np.array([0.1e-19, 9616.72, 615.511, 1.11186])#4
     names = ['Ymin', 'Ymax', 'EC50', 'nH']
     notfixed = np.array([False, True, True, True])
     fixval = copy.deepcopy(pars)
-    minssd = equations.SSD(pars, func, dataset, notfixed, fixval)
-    print '\n SSD \n', minssd
-    hes = errors.hessian(dataset, equations.hill_equation, pars, notfixed, fixval)
+    Smin = SSD(pars, hill_equation, dataset)
+    print '\n SSD \n', Smin
+    hes = errors.hessian(dataset, hill_equation, pars, notfixed)
     print '\n Observed information matrix = \n', hes
-    covar = errors.covariance_matrix(dataset, equations.hill_equation, pars, notfixed, fixval) 
+    covar = errors.covariance_matrix(dataset, hill_equation, pars, notfixed) 
     print '\n Covariance matrix = \n', covar
-    aSD = errors.approximateSD(dataset, equations.hill_equation, pars, notfixed, fixval)
-    print '\n aSD \n', aSD
+    aproxSD = errors.approximateSD(dataset, hill_equation, pars, notfixed)
+    #print '\n aproximate SD = \n', aSD
+    
+    print '\n'
+    j = 0
+    for i in range(len(names)):
+        str = 'Parameter {0:d}: {1}\t= {2:.3f}\t'.format(i+1, names[i], pars[i])
+        if notfixed[i]:
+            str += 'Approx SD = {0:.3f}'.format(aproxSD[j])
+            j += 1
+        else:
+            str += '(fixed)'
+        print str
+    
     correl = errors.correlation_matrix(covar)
     print '\n Correlation matrix = \n', correl
     
     
-    #lowli = errors.confidence_interval(covar, dataset, equations.hill_equation, pars, notfixed)
-    ssec, ssec1, ssec2, count = errors.new_secs3(aSD, covar, dataset, equations.hill_equation, pars, notfixed, hes)
-    print '\n ssec = \n', ssec, ssec1, ssec2
-    print '\n count = \n', count    
-    #print '\n aasdlist = \n', aasdlist  
-    fitpars = pars[1:]
-    names = names[1:]
-    for name, var, sd in zip(names, fitpars, aSD):
-        print ('\n{0} = {1:.3g} +/- {2:.3g}'.format(name, var, sd))
+    # Number of points fitted = nfit
+    nfit = dataset.size()
+    # Number of parameters estimated = kfit
+    kfit = np.nonzero(notfixed)[0].size
+    # Degree of freedom = ndf
+    ndf = nfit - kfit
+    print('\nNumber of point fitted = {0:d}'.format(nfit))
+    print('Number of parameters estimated = {0:d}'.format(kfit))
+    print('Degrees of freedom = {0:d}'.format(ndf))
     
+    var = Smin / ndf
+    Sres, Lmax = sqrt(var), -SSDlik(pars, hill_equation, dataset, notfixed)
+    print ('\nResidual error SD = {0:.3f} (variance = {1:.3f})'.format(Sres, var))
+    print ('Minimum SSD = {0:.3f}; \tMax log-likelihood = {1:.3f}'.format(Smin, Lmax))
+    
+    tval = errors.tvalue(ndf)
+    m = tval * tval / 2.0
+    print "\ntval = {0:.3g}; m = {1:.3g}".format(tval, m)
+    clim = sqrt(2. * m)
+    print 'Equivalent SD for Gaussian:', clim
+    Lcrit = Lmax - m
+    print 'Lmax=', Lmax, 'Lcrit=', Lcrit
+    
+    
+
+    Llimits = errors.lik_intervals(names, pars, aproxSD, 
+        notfixed, m, hill_equation, dataset)
+    print Llimits