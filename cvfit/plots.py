27c24f90e92f8427861cea3bfa5f65866f763f2b
remislp <r.lape@ucl.ac.uk>
2015-02-11 22:07:46 +0000

    Removed dependency to prettyplotlib

--- cvfit/plots.py
+++ cvfit/plots.py
@@ -2,7 +2,6 @@ import os
 import math
 import numpy as np
 from pylab import *
-import prettyplotlib as ppl
 
 def plot_hill_fit_result_single(filename, set, equation, 
     plotdata=True, plotfit=True, plotguess=False, plotaverage=False,
@@ -14,35 +13,32 @@ def plot_hill_fit_result_single(filename, set, equation,
     fig, ax = subplots()
     ax.set_xscale('log')
     ax.set_title(os.path.split(filename)[1][:-4] + ' ' + set.title)
-    handles = []
     
     # Plot original data
     if plotdata:
-        pl1, = ax.plot(set.X, set.Y, 'ko', label='experimental data')
-        handles.append(pl1)
+        ax.plot(set.X, set.Y, 'ko', label='experimental data')
+
     # Plot average data.
     if plotaverage:
-        pl4, = ax.plot(set.avX, set.avY, 'ro', label='average')
+        ax.plot(set.avX, set.avY, 'ro', label='average')
         ax.errorbar(set.avX, set.avY, yerr=set.avS, fmt='none', ecolor='r')
-        handles.append(pl4)
+
     # Plot guess curve
     if plotguess:
         logplotX = np.log10(set.X)
         plotX = 10 ** np.linspace(np.floor(np.amin(logplotX) - 1),
                                   np.ceil(np.amax(logplotX) + 1), 100)
         plotYg = equation.equation(plotX, equation.guess)
-        pl2, = ax.plot(plotX, plotYg, 'y-', label='guess')
-        handles.append(pl2)
+        ax.plot(plotX, plotYg, 'y-', label='guess')
     # Plot fitted curve
     if plotfit:
         logplotX = np.log10(set.X)
         plotX = 10 ** np.linspace(np.floor(np.amin(logplotX) - 1),
                                   np.ceil(np.amax(logplotX) + 1), 100)
         plotYr = equation.equation(plotX, equation.pars)
-        pl3, = ax.plot(plotX, plotYr, 'b-', label='fit')
-        handles.append(pl3)
-    
-    legend(handles=handles, loc='lower right')
+        ax.plot(plotX, plotYr, 'b-', label='fit')
+
+    legend(loc=2)
     
     if save_fig:
         fig.savefig(save_name)
@@ -57,7 +53,6 @@ def plot_hill_fit_result_multiple(fname, fs, norm=False, save_fig=False, save_na
     type = ''
     if norm:
         type = ' (normalised)'
-        
     ax.set_title(os.path.split(fname)[1][:-4]+type)
     
     # Generate the common x axis
@@ -65,22 +60,17 @@ def plot_hill_fit_result_multiple(fname, fs, norm=False, save_fig=False, save_na
     xmax = math.ceil(math.log10(max([s.data.Xmax() for s in fs])))
     plotx = 10 ** np.linspace(xmin, xmax, 100)
     
-    handles = []
     for index, set in enumerate(fs):
         if norm:
-            Y = set.data.normY
-            pars = set.eq.normpars
+            Y, pars = set.data.normY, set.eq.normpars
         else:
-            Y = set.data.Y
-            pars = set.eq.pars
+            Y, pars = set.data.Y, set.eq.pars
         # Plot original data and fitted curve
-        pl1, = ax.plot(set.data.X, Y,
-                 'o', color=ppl.colors.set2[index], label=set.data.title)
-        pl2, = ax.plot(plotx, set.eq.equation(plotx, pars),
-                 color=ppl.colors.set2[index], label=(set.data.title+' fit'))
-        handles.extend([pl1, pl2])
-    legend(handles=handles, loc='upper left')
-    
+        ax.plot(set.data.X, Y, 'o', label=set.data.title)
+        ax.plot(plotx, set.eq.equation(plotx, pars), 
+            label=(set.data.title+' fit'))
+                
+    legend(loc=2)
     if save_fig:
         fig.savefig(save_name)
         close(fig)
